f14cafdd383d3c609580068c4dbb49b13f59dd33
Michael Costa dos Reis <michael@gia.com.br>
2022-04-28 21:30:42 -0300

    pagina inicial estilizada

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,33 @@
-document.querySelector("#escolha-radio").addEventListener('click',function(){
-    console.log('clicou');
+var noticias = [
+    {
+        titulo : "Com Parkinson, Ozzy Osbourne adapta própria mansão",
+        data:"28 de abril de 2022",
+        descritivo:"Ozzy Osbourne, que revelou sofrer da Doença de Parkinson há 17 anos, afirmou que a mansão, Welders House, que fica em Buckinghamshire, na Inglaterra, foi totalmente adaptada com barras de apoio e superfícies antiderrapantes para atender as necessidades de saúde dele. As informações são do Daily Mail. O cantor, de 73 anos, ressaltou que conta com uma equipe de enfermagem em tempo integral para atendê-lo, além disso, tem um estúdio de exercício e bem-estar.",
+        radio : "caxias"
+    },
+    {
+        titulo:"Guns N’ Roses volta às paradas após Thor; entenda",
+        data:"28 de abril de 2022",
+        radio : "caxias",
+        descritivo:"O Guns N’ Roses voltou às paradas da Billboard. A faixa “Sweet Child O’ Mine”, do disco “Appetite for Destruction” (1987), é a trilha sonora do primeiro trailer de “Thor: Amor e Trovão” (2022), e ganhou repercussão em rádios e na internet. Segundo a Billboard, música entrou na Hot Hard Rock Songs em primeiro lugar, e na Hot Rock & Alternative Songs, na nona posição."
+    },
+    {
+        titulo:"The Boys: 3ª temporada ganha teaser eletrizante; veja",
+        data:"28 de abril de 2022",
+        radio : "bento",
+        descritivo:"E vem aí! Nas primeiras horas desta quarta-feira (27), a terceira temporada de “The Boys” ganhou uma prévia eletrizante e instigante. O teaser começa com um anúncio da Vought, que logo se transforma em uma mega cena de ação com explosões em que Butcher reúne todo seu grupo para atacar os “heróis”."
+    }
+];
+
+var links = document.querySelectorAll("#escolha-radio li");
+links.forEach(function(link){
+    link.addEventListener('click', function(ev){
+        ev.preventDefault();
+        localStorage.setItem('radio', ev.target.dataset.radio);
+    });
 });
+
+
+function obterNoticiasPorRadio(radio){
+    return noticias.filter(noticia => noticia.radio == radio);
+}
